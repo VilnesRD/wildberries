aa797197815fac2e098fd3ecd3c734ef8faedbf9
Dmitry Rodichev <rdp9000@gmail.com>
2023-01-20 17:46:43 +0700

    Update all project

--- src/test/java/wildberries/TestBase.java
+++ src/test/java/wildberries/TestBase.java
@@ -1,23 +1,38 @@
 package wildberries;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
-import static com.codeborne.selenide.Selenide.open;
+import wildberries.config.ProjectConfiguration;
+import wildberries.config.WebConfig;
+import wildberries.helpers.Attach;
 
 public class TestBase {
+
+    private static WebConfig config;
+    private static ProjectConfiguration configuration;
     @BeforeAll
-    static void setUP() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1920x1080";
+    static void setUp() {
+        Configuration.timeout = 10000;
+        config = ConfigFactory.create(WebConfig.class, System.getProperties());
+        configuration = new ProjectConfiguration();
+        configuration.webConfig(config);
     }
 
     @BeforeEach
-    void config() throws InterruptedException {
-        open("https://www.wildberries.ru/");
-        Configuration.timeout = 10000;
-        Thread.sleep(4000);
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
-
--- src/test/java/wildberries/WebTest.java
+++ src/test/java/wildberries/WebTest.java
@@ -2,62 +2,88 @@ package wildberries;
 
 import com.codeborne.selenide.Condition;
 
-
-import io.qameta.allure.Severity;
-import io.qameta.allure.Step;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class WebTest extends TestBase {
 
-
     @Test
-    void currencyTest() throws InterruptedException {
-        $("ul.header__simple-menu li").hover();
-        $("form.popup__form label", 2).click();
-        Thread.sleep(3000); //понимаю, что bad practice, но без этого не работает
-        $("#searchInput").click();
-        $("#searchInput").setValue("iphone");
-        $("#applySearchBtn").click();
-        $(".product-card__brand").shouldBe(Condition.visible);
-        $("div.product-card__price span").shouldHave(Condition.text("тг."));
-
+    @Owner("Dmitry Rodichev")
+    @Severity(value = io.qameta.allure.SeverityLevel.CRITICAL)
+    @DisplayName("Проверка смены валюты в карточке товара")
+    void currencyTest() {
+        step("Выбираем валюту", () -> {
+            $("ul.header__simple-menu li").hover();
+            $("form.popup__form label", 2).click();
+        });
+        step("В поисковой строке вводим название товара", () -> {
+            $("#searchInput").shouldBe(Condition.visible, Duration.ofSeconds(15)).click();
+            $("#searchInput").setValue("iphone");
+            $("#applySearchBtn").click();
+        });
+        step("Проверяем, что цена товара отображается в выбранной валюте", () -> {
+            $(byCssSelector("div.product-card__price ins")).shouldHave(Condition.text("тг."));
+        });
     }
 
-
     @Test
+    @Owner("Dmitry Rodichev")
+    @Severity(value = io.qameta.allure.SeverityLevel.CRITICAL)
+    @DisplayName("Проверка поиска по картинке")
     void pictureSearchTest() {
-        $("#searchByImageContainer button").click();
-        $("label.upload-photo-btn input").uploadFromClasspath("s22.jpeg");
-        $(".product-card__brand").$("p.product-card__brand-name span").shouldHave(Condition.text("Samsung"));
+        step("В поисковой строке выбираем поиск по изображению", () -> {
+            $("#searchByImageContainer button").click();
+        });
+        step("Загружаем картинку", () -> {
+            $("label.upload-photo-btn input").uploadFromClasspath("s22.jpeg");
+        });
+        step("Проверяем, что выдача поиска содержит товары", () -> {
+            $(".product-card__brand").$("p.product-card__brand-name span")
+                    .shouldHave(Condition.text("Samsung"));
+        });
     }
 
 
     @Test
+    @Owner("Dmitry Rodichev")
+    @Severity(value = io.qameta.allure.SeverityLevel.CRITICAL)
+    @DisplayName("Проверка открытия страницы приложения wb в google play")
     void appLinkTest() {
-        $("a.google-play").click();
-        switchTo().window(1);
-        assertEquals("https://play.google.com/store/apps/details?id=com.wildberries.ru", url());
+        step("Переходим на страницу приложения wb в google play по ссылке на сайте wb", () -> {
+            $("a.google-play").click();
+            switchTo().window(1);
+        });
+        step("Проверяем, что открылась страница приложения wb в google play", () -> {
+            assertEquals("https://play.google.com/store/apps/details?id=com.wildberries.ru", url());
+        });
     }
 
 
-    @ValueSource(strings = {"Брюки", "Верхняя одежда", "Джемперы, водолазки и кардиганы", "Джинсы", "Комбинезоны", "Костюмы",
-            "Лонгсливы", "Пиджаки, жилеты и жакеты", "Платья и сарафаны", "Толстовки, свитшоты и худи", "Туники",
-            "Футболки и топы", "Халаты", "Шорты", "Юбки", "Белье", "Большие размеры", "Будущие мамы", "Для высоких",
-            "Для невысоких", "Одежда для дома", "Офис", "Пляжная мода", "Религиозная", "Свадьба", "Спецодежда и СИЗы",
-            "Подарки женщинам"})
-    @ParameterizedTest(name = "Проврка наличия вкладок с названием {0} в разделе Женщинам")
+    @CsvFileSource(resources = "/womanClothes.csv")
+    @ParameterizedTest(name = "Проверка наличия вкладок с названием {0} в разделе Женщинам")
+    @Owner("Dmitry Rodichev")
+    @Severity(value = SeverityLevel.NORMAL)
+    @DisplayName("Проверка наличия вкладок в разделе Женщинам")
     void womanBurgerMenuTest(String name) {
-        $(".nav-element__burger").click();
-        $("ul.menu-burger__main-list li").click();
-        $(".menu-catalog__list-2").shouldHave(Condition.text(name));
+        step("Переходим на страницу Женщинам", () -> {
+            $(".nav-element__burger").shouldBe(Condition.visible,Duration.ofSeconds(8)).click();
+            $("ul.menu-burger__main-list li").click();
+        });
+        step("Проверяем, что открылась страница Женщинам со списком вкладок {0}", () -> {
+            $(".menu-catalog__list-2").shouldHave(Condition.text(name));
+        });
     }
 
 
@@ -66,13 +92,18 @@ public class WebTest extends TestBase {
             "s21, Galaxy",
     })
     @ParameterizedTest
-    void searchTest(String search, String expected) throws InterruptedException {
-        Thread.sleep(5000);
-        $("#searchInput").click();
-        $("#searchInput").setValue(search);
-        $("#applySearchBtn").click();
-        $(".product-card__brand").$("p.product-card__brand-name span", 1)
-                .shouldHave(Condition.text(expected));
-
+    @Owner("Dmitry Rodichev")
+    @Severity(value = SeverityLevel.NORMAL)
+    @DisplayName("Проверка работы поиска при введении названия товара")
+    void searchTest(String search, String expected) {
+        step("Вводим в строку поиска название товара", () -> {
+            $("#searchInput").shouldBe(Condition.visible, Duration.ofSeconds(15)).click();
+            $("#searchInput").setValue(search);
+            $("#applySearchBtn").click();
+        });
+        step("Проверяем, что открылась страница с , соответствующего производителя", () -> {
+            $(".product-card__brand").$("p.product-card__brand-name span", 1)
+                    .shouldHave(Condition.text(expected));
+        });
     }
 }
