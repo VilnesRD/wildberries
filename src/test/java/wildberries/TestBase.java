1a4d9b5e2bb0bd7a8f814c71877e2307706bf8d0
Dmitry Rodichev <rdp9000@gmail.com>
2023-01-23 15:03:26 +0700

    Update all project

--- src/test/java/wildberries/TestBase.java
+++ src/test/java/wildberries/TestBase.java
@@ -1,27 +1,43 @@
 package wildberries;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import github.config.ConfigReader;
-import github.config.ProjectConfiguration;
-import github.config.WebConfig;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import wildberries.helpers.Attach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+public class TestBase {
 
-public class BaseTest {
-
-    private static final WebConfig webConfig = ConfigReader.read();
     @BeforeAll
-    public static void setUp() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        ProjectConfiguration projectConfiguration = new ProjectConfiguration(webConfig);
-        projectConfiguration.webConfig();
+    public static void testBaseUrlConfiguration() {
+        Configuration.pageLoadTimeout = 50000;
+        Configuration.baseUrl = System.getProperty("baseUrl","https://rit-it.com");
+        Configuration.browserSize = System.getProperty("resolution", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.remote = System.getProperty("selenoideURL", "https://user1:1234@selenoid.autotests.cloud") + "/wd/hub";
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
-    public void tearDown() {
-        closeWebDriver();
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
     }
 }
--- src/test/java/wildberries/helpers/Attach.java
+++ src/test/java/wildberries/helpers/Attach.java
@@ -1,4 +1,4 @@
-package wildberries;
+package wildberries.helpers;
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
